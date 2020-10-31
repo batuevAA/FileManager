f099edbd0a717d9525918c55e2c3b73bf978d6d2
batuevAA <artbatu@yandex.ru>
2020-10-31 17:09:44 +0500

    Доделал обработку ошибок для метода send в api.js

--- src/server/api.js
+++ src/server/api.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-
 import { showLoader } from '../load.js'
 import { hideLoader } from '../load.js'
 
@@ -8,56 +7,59 @@ export class Api {
     }
   
     async send(url, value) {
-      try {
-        const resp = await axios.post(url, value);
-
-        return resp.data;
-      } catch(err) {
-        console.log('Ошибка при отправке данных на сервер в функции api/send ' + err);
-        throw err;
-      }
+        let resp = {};
+        try {
+            resp = await axios.post(url, value);
+            return resp.data;
+        } catch(err) {
+            resp.errorCode = 7;
+            resp.errorText = err.message;
+            console.log('Ошибка при отправке запроса ' + err.message);
+            return resp;
+        }
+      
     }
   
     async copy(source, dist) {
-      showLoader();
-      const result = await this.send('/copy', {
-        file: source, 
-        dist: dist
-      })
-      hideLoader();
-      return result;
+        showLoader();
+        const result = await this.send('/copy', {
+            file: source, 
+            dist: dist
+        })
+        hideLoader();
+        return result;
     }
 
 
     async delete(targetFile) {
-      showLoader();
-      const result = await this.send('/delete', {
-        file: targetFile
-      })
-      hideLoader();
-      return result;
+        showLoader();
+        const result = await this.send('/delete', {
+            file: targetFile
+        })
+        hideLoader();
+        return result;
     }
 
     async move(source, dist) {
-      showLoader();
-      const result = await this.send('/move', {
-        file: source, 
-        dist: dist
-      })
-      hideLoader();
-      return result;
+        showLoader();
+        const result = await this.send('/move', {
+            file: source, 
+            dist: dist
+        })
+        hideLoader();
+        return result;
     }
   
     async changeDirectory(dirName) {
-      const result = await this.send('/changeDir', {
-        dir: dirName
-      })
-      return result;
+        const result = await this.send('/changeDir', {
+            dir: dirName
+        })
+        return result;
     }
 
     async getDiskList() {
-      const result = await this.send('/disk')
-      return result;
+        const result = await this.send('/disk')
+        return result;
     }
   
-  }
+}
